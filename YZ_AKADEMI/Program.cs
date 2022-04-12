27d15f5eb5fa773dcdf0545a16ba77319e4dd70a
haticenur.4455 <66225075+HNK-hub@users.noreply.github.com>
2022-04-13 02:30:57 +0300

    Add project files.

---
+++ YZ_AKADEMI/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YZ_AKADEMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] AlicePoint = new int[3];
+            int[] BobPoint = new int[3];
+            int BobSkor =0;
+            int AliceSkor = 0;
+
+            //ALİCE PROBLEM
+            for (int i = 0; i < 3; i++)
+
+            {
+                Console.Write("Alice Puan giriniz = ");
+
+                AlicePoint[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //BOB PROBLEM
+            for (int i = 0; i < 3; i++)
+
+            {
+                Console.Write("Bob Puan giriniz = ");
+
+                BobPoint[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(AlicePoint[i] + " ");
+
+            }
+            Console.Write("\n");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(BobPoint[i] + " ");
+
+            }
+            Console.ReadKey();
+
+            //Islem
+            for (int i = 0; i < 3; i++)
+            {
+                if (AlicePoint[i] < BobPoint[i])
+                    AliceSkor++;
+                if (AlicePoint[i] > BobPoint[i])
+                    BobSkor++;
+                if (AlicePoint[i] == BobPoint[i])
+                    AliceSkor = 0;
+                BobSkor = 0;
+
+            }
+            Console.Write(AliceSkor + " ", BobSkor + " ");
+
+
+
+
+        }
+
+
+
+    }
+    }
