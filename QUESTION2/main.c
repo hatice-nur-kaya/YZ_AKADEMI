2db4e92eda0740c35e8683fd0a89e2372919b4c8
Hatice Nur KAYA <66225075+HNK-hub@users.noreply.github.com>
2022-04-25 01:10:50 +0300

    Add files via upload
    Question2 C dili ile yapılmıştır Dizi mantığı kullanılmıştır.

---
+++ QUESTION2/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+char time[][21] = {"o' clock", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+
+int main() {
+
+    int minutes, hours;
+    int half = 0;
+    scanf("%d %d", &hours, &minutes);
+
+    if (minutes > 30) {
+        minutes = 60-minutes;
+        half = 1;
+    }
+
+    if (minutes < 21 && minutes != 15 && minutes != 0 && minutes != 1) {
+        printf("%s minutes ", time[minutes]);
+    }
+    else if (minutes == 1) {
+        printf("%s minute ", time[minutes]);
+    }
+    else if (minutes > 20 && minutes < 30) {
+        printf("%s %s minutes ", time[20], time[minutes-20]);
+    }
+    else if (minutes == 15) {
+        printf("quarter ");
+    }
+    else if (minutes == 30) {
+        printf("half ");
+    }
+
+    if (half && minutes != 0) {
+        printf("to %s", time[hours+1]);
+    }
+    else if (!half && minutes != 0){
+        printf("past %s", time[hours]);
+    }
+    else {
+        printf("%s %s", time[hours], time[0]);
+    }
+
+
+    return 0;
+}
